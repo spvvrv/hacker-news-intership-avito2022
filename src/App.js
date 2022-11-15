56e27d1914406a0d7942e3d4da31a096598ef29c
spvvrv <s.pivovarova@rambler.ru>
2022-11-15 17:49:17 +0300

    feat: add yarn, react-app, redux, Material UI

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+}
+
+export default App;
